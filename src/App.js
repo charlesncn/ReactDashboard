dffcc519e31a81bca433d169944494a228bdb97f
charlesncn <charowcjn@gmail.com>
2022-10-03 00:00:08 +0300

    React-dom setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import {
+  BrowserRouter, 
+  Routes,
+  Route,
+} from 'react-router-dom'
+import Login from './pages/login/Login';
+import Home from './pages/home/Home';
+import List from './pages/list/List';
+import Single from './pages/single/Single';
+import New from './pages/new/New';
 
 function App() {
   return (
     <div className="App">
-     
+     <BrowserRouter>
+      <Routes path='/'>
+        <Route index element={<Home/>}></Route>
+        <Route path='login' element={<Login/>}></Route>
+        <Route path='user'>
+        <Route index element={<List/>}/>
+          <Route path=':userid' element={<Single/>}/>
+          <Route path='new' element={<New/>}/>
+        </Route>
+      </Routes>
+     </BrowserRouter>
     </div>
   );
 }
